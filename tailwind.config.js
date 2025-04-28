3778fed1d7a45c7a33f8bb9921f9de333bd96128
MarceloHaa <marcelohenrique.a@hotmail.com>
2025-04-28 16:28:25 -0300

    Changing the project root and installing tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#27374D",
+        secondary: "#526D82",
+        accent: "#9DB2BF",
+        light: "#DDE6ED",
+        dark: "#3b413c",
+        neutral: "#9db5b2",
+      },
+    },
+  },
+  plugins: [],
+};
