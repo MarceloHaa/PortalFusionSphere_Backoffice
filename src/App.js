24c1c4915c3f2c42bdf020b4c9f1362e6e1463e6
MarceloHaa <marcelohenrique.a@hotmail.com>
2025-04-29 19:13:27 -0300

    created login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
     </Router>
   );
 }
